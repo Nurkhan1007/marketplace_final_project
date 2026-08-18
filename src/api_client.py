2c9bf3df14758a4b0cde3eb62d53e2c8fd27adcd
Nurkhan <nabdikulov80@gmail.com>
2026-08-18 17:17:35 +0500

    fix: add retry for API timeouts

--- src/api_client.py
+++ src/api_client.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 from logger import setup_logging
 import pandas as pd
+from time import sleep
 
 logger = setup_logging()
 
@@ -11,17 +12,27 @@ load_dotenv(os.path.join(parent_dir, '.env'))
 
 API_URL = os.getenv('API_URL')
 
+max_attempts = 2
+reques_timeout = 60
+retry_delay = 10
+
 def get_data(target_date):
     if not API_URL:
         raise ValueError('Переменная API_URL не найдена в .env')
-    try:
-        response = requests.get(API_URL, params = {'date':target_date}, timeout=30)
-        logger.info(f'Дата:{target_date}, HTTP статус: {response.status_code}')
-        response.raise_for_status()
-        data = response.json()
-    except requests.exceptions.RequestException:
-            logger.exception(f'Ошибка запроса за дату {target_date}')
-            raise
+    for attempt in range(1, max_attempts+1):
+        try:
+            logger.info(f'Запрос данных за {target_date}. Попытка {attempt}/{max_attempts}')
+            response = requests.get(API_URL, params = {'date':target_date}, timeout=reques_timeout)
+            logger.info(f'Дата:{target_date}, HTTP статус: {response.status_code}')
+            response.raise_for_status()
+            data = response.json()
+            break
+        except requests.exceptions.RequestException as error:
+                if attempt == max_attempts:
+                    logger.exception(f'Запрос данных за {target_date} завершился ошибкой после {max_attempts} попыток')
+                    raise
+                logger.warning(f'Ошибка запроса данных за {target_date}:{error}. Повтор через {retry_delay} секунд.')
+                sleep(retry_delay)
     if not isinstance(data, list):
          raise ValueError(f'За {target_date} API вернул неожиданный тип: {type(data).__name__}')
 
